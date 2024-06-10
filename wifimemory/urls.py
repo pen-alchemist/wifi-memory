3c1adfa7f3f4c1e8b4b016e3aab3c1fa8b6f6ad9
Egor Romanov <alchemy.follower666@gmail.com>
2024-06-10 16:56:50 +0300

    VERSION 0.0.1 WiFi scanner 5/10. Wifi signal status 5/10, IP address detector 5/10.

---
+++ wifimemory/urls.py
@@ -0,0 +1,25 @@
+"""
+URL configuration for wifimemory project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    wifimemory. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    wifimemory. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    wifimemory. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+from application import urls
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(urls)),
+]
