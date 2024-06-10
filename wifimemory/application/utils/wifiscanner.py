5fb27d2bdde4705b6b4616592f5901b9a17d548a
Egor Romanov <alchemy.follower666@gmail.com>
2024-06-10 12:38:35 +0300

    VERSION 0.0.1 WiFi scanner 1/10.

--- wifimemory/application/utils/wifiscanner.py
+++ wifimemory/application/utils/wifiscanner.py
@@ -1,11 +1,34 @@
 from scapy.all import *
-from threading import Thread
-import pandas
-import time
-import os
 
 
-# initialize the networks dataframe that will contain all access points nearby
-networks = pandas.DataFrame(columns=["BSSID", "SSID", "dBm_Signal", "Channel", "Crypto"])
-# set the index BSSID (MAC address of the AP)
-networks.set_index("BSSID", inplace=True)
+def sniff_beacon(iface):
+    """Sniffs for beacon frames and displays network information."""
+    print("Scanning WiFi networks...")
+    # Filter for beacon frames (type 0, subtype 8)
+    # promisc=True enables monitor mode (requires root)
+    packets = sniff(iface=iface, count=10, filter="type 0 and subtype 8", promisc=True)
+
+    for packet in packets:
+        # Extract SSID (network name) from beacon frame
+        ssid = packet[Dot11Elt].info
+        # Extract BSSID (MAC address of access point)
+        bssid = packet[Dot11].addr2
+        # Extract channel
+        channel = packet[Radio].channel
+        # Extract signal strength (received signal strength indication - RSSI)
+        rssi = packet.dBm_antsignal
+
+        print(f"SSID: {ssid}")
+        print(f"BSSID: {bssid}")
+        print(f"Channel: {channel}")
+        print(f"Signal Strength (RSSI): {rssi} dBm")
+        print("-" * 30)
+
+
+# Get user input for network interface
+iface = input("Enter your wireless interface (e.g., wlan0): ")
+
+# Start sniffing for beacons
+sniff_beacon(iface)
+
+print("Scan completed!")
